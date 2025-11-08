ea7460ae882e227fba478634334348bac9151117
Meerthika <meerthikasrrajendrans@gmail.com>
2025-11-08 17:26:53 +0530

    Move packet to its header file

--- main.c
+++ main.c
@@ -2,17 +2,7 @@
 #include <stdint.h>
 
 
-#pragma pack(push, 1)
-typedef struct frame
-{
-    uint8_t preamble; 
-    uint16_t seq_no;
-    uint16_t ack_no;
-    uint8_t data[512];
-    uint8_t checksum;
 
-};
-#pragma pack(pop)
 
 
 
