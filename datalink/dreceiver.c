3cd46934925c1d7720c8dd03f568df7408b8b237
Meerthika <meerthikasrrajendrans@gmail.com>
2025-11-09 19:24:12 +0530

    Implement dreceiver function

--- datalink/dreceiver.c
+++ datalink/dreceiver.c
@@ -9,7 +9,26 @@
 #include <stdlib.h> 
 #include "../include/datalink.h"
 
-int dsender(){
+int dreceiver(){
+
+    int fd = setup_termios("/dev/ttys007");
+    frame_t f;
+
+    receive_frame(fd, &f);
+
+    //compute crc again and check
+    char bitstring[1024], remainder[32];
+    string_to_bits((char*)f.data, bitstring);
+    char divisor[] = "1101"; // same as sender
+    compute_crc(bitstring, divisor, remainder);
+
+    int received_crc = (uint8_t)strtol(remainder, NULL, 2);
+
+    if(received_crc == f.crc){
+        printf("No Error in received data");
+    }else{
+        printf("Error in Received Data");
+    }
 
     return 0;
 }
