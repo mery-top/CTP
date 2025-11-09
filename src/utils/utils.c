87f05cdd805455a4f12e14c849b832e81d2e9e74
Meerthika <meerthikasrrajendrans@gmail.com>
2025-11-09 17:15:01 +0530

    Add string to bits function for crc

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -1,1 +1,16 @@
 #include<stdio.h>
+
+
+void string_to_bits(char* message, char* output){
+    int k =0;
+    for(int i =0; i< strlen(message);i++){
+        for(int j =7; j>=0; j--){
+            unsigned char c = message[i]; //A->75
+
+            //1. left shift 1 by j to check the first bit and then sequentailly the next bits
+            output[k++] = ((c & 1<<j) )? '1' : '0';
+        }
+    }
+
+    output[k] = '\0';
+}
