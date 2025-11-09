297a790378f1f346f83d5f1335f2373aa92e237f
Meerthika <meerthikasrrajendrans@gmail.com>
2025-11-09 19:12:51 +0530

    Import header files in dreceiver

---
+++ datalink/dreceiver.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "../include/comm.h"
+#include "../include/crc.h"
+#include "../include/packet.h"
+#include "../include/termios.h"
+#include "../include/ack.h"
+#include <string.h>
+#include <stdlib.h> 
+#include "../include/datalink.h"
+
+int dsender(){
+
+    return 0;
+}
